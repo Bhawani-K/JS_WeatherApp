2c1bf610e48814f72eae39b3f7b57a735d150b08
Bhawani_K <kundala.bhawani@outlook.com>
2023-01-29 23:25:21 +0530

    added time & date Feature

--- script.js
+++ script.js
@@ -1,26 +1,63 @@
-const API_KEY = '827ab4bb33bebecd5a5d82e5a4517a7d'
+const timeEle = document.getElementById('time');
+const dateEle = document.getElementById('date')
+const currentWeatherItemsEle = document.getElementById('currentWeatherItems')
+const timezoneEle = document.getElementById('time-zone')
+const countryEle = document.getElementById('country')
+const weatherForecastEle = document.getElementById('weather-forecast')
+const currentTempEle = document.getElementById('current-temp')
+const weatherIconEle = document.getElementById('w-icon')
+
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+]
+const months = [
+    'Jan', 'Feb', 'March', 'Apr', 'May', 'June', 'July',
+    'Aug', 'Sept', 'Oct', 'Nov', 'Dec'
+]
 let cityName = 'Brahmapur';
 function getWeatherData(cityName) {
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
+    
+    let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`
     // Fetch the data from the OpenWeatherMap API
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            // Extract the weather data
-            var temperature = data.main.temp;
-            var humidity = data.main.humidity;
-            var weather = data.weather[0].description;
+    setInterval(() => {
+        fetch(API_URL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                // Extract & convert date
+                const date = new Date(data.dt * 1000);
+                const month = date.getMonth()
+                const day = date.getDay()
+                const getdate = date.getDate()
+                // get WeatherDetails
+                weatherIconEle.innerHTML= data.weather[0].icon
+                // const dateOnly = date.toLocaleDateString();
+                const timeOnly = date.toLocaleTimeString();
+                dateEle.innerHTML = days[day] + ',' + getdate + months[month];
+                timeEle.innerHTML = timeOnly;
+                // Latitude & Longitude
+                const getLat = data.coord.lat;
+                const getLon = data.coord.lon;
+                // Get Location Details
+                fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${getLat}&longitude=${getLon}&localityLanguage=en`)
+                .then(response => response.json())
+                .then(function(dataEle){
+                    timezoneEle.innerHTML = dataEle.city+'<br>' +dataEle.principalSubdivision
+                    countryEle.innerHTML = dataEle.countryName
+                })
+            })
+            .catch(function (error) {
+                console.log("Error: " + error);
+            });
+    }, 1000)
 
-            // Log the weather data to the console
-            console.log("Temperature: " + temperature + "°C");
-            console.log("Humidity: " + humidity + "%");
-            console.log("Weather: " + weather);
-        })
-        .catch(function (error) {
-            console.log("Error: " + error);
-        });
 }
 getWeatherData(cityName)
